ef5b0ab66e6089feadb98679450b8e5650c543ac
shahidfoy <shahidfoy@gmail.com>
2021-08-31 15:27:43 -0600

    created contract and offer service

--- src/app/service/contract.service.ts
+++ src/app/service/contract.service.ts
@@ -1,9 +1,58 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Contract } from '../model/contract';
+import { CustomHttpResponse } from '../model/custom-http-response';
+import { Offer } from '../model/offer';
+import { User } from '../model/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContractService {
 
-  constructor() { }
+  private host = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  public addContract(contract: Contract): Observable<Contract> {
+    return this.http.post<Contract>(`${this.host}/contract/add`, contract);
+  }
+
+  public assignContract(id: number, contractor: User, offer: Offer): Observable<Contract> {
+    return this.http.post<Contract>(`${this.host}/contract/assign/${id}`, {contractor, offer});
+  }
+
+  public deleteContract(id: number): Observable<CustomHttpResponse> {
+    return this.http.delete<CustomHttpResponse>(`${this.host}/contract/delete/${id}`);
+  }
+
+  public editContract(contract: Contract): Observable<Contract> {
+    return this.http.post<Contract>(`${this.host}/contract/edit`, contract);
+  }
+
+  public findContractById(id: number): Observable<Contract> {
+    return this.http.get<Contract>(`${this.host}/contract/${id}`);
+  }
+
+  public findContractByStatus(status: string): Observable<Contract[]> {
+    return this.http.get<Contract[]>(`${this.host}/contract/find/status/${status}`);
+  }
+
+  public findContractByType(type: string): Observable<Contract[]> {
+    return this.http.get<Contract[]>(`${this.host}/contract/find/type/${type}`);
+  }
+
+  public findContractByContractee(contractee: User): Observable<Contract[]> {
+    return this.http.post<Contract[]>(`${this.host}/contract/find/contractee`, contractee);
+  }
+
+  public findContractByContractor(contractor: User): Observable<Contract[]> {
+    return this.http.post<Contract[]>(`${this.host}/contract/find/contractor`, contractor);
+  }
+
+  public getContracts(): Observable<Contract[]> {
+    return this.http.get<Contract[]>(`${this.host}/contract/find/all`);
+  }
 }
--- src/app/service/offer.service.ts
+++ src/app/service/offer.service.ts
@@ -1,9 +1,52 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { CustomHttpResponse } from '../model/custom-http-response';
+import { Offer } from '../model/offer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OfferService {
 
-  constructor() { }
+  private host = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  public addOffer(offer: Offer): Observable<Offer> {
+    return this.http.post<Offer>(`${this.host}/offer/add`, offer);
+  }
+
+  public deleteOffer(id: number): Observable<CustomHttpResponse> {
+    return this.http.delete<CustomHttpResponse>(`${this.host}/offer/delete/${id}`);
+  }
+
+  public editOffer(offer: Offer): Observable<Offer> {
+    return this.http.post<Offer>(`${this.host}/offer/edit`, offer);
+  }
+
+  public approveOffer(id: number, approved: boolean): Observable<Offer> {
+    return this.http.post<Offer>(`${this.host}/offer/approve/${id}/${approved}`, {});
+  }
+
+  public findOfferById(id: number): Observable<Offer> {
+    return this.http.get<Offer>(`${this.host}/offer/${id}`);
+  }
+
+  public findOfferByContractId(id: number): Observable<Offer[]> {
+    return this.http.get<Offer[]>(`${this.host}/offer/find/contract/${id}`);
+  }
+
+  public findOfferByUserId(id: number): Observable<Offer[]> {
+    return this.http.get<Offer[]>(`${this.host}/offer/find/user/${id}`);
+  }
+
+  public findOfferByUsername(username: string): Observable<Offer[]> {
+    return this.http.get<Offer[]>(`${this.host}/offer/find/username/${username}`);
+  }
+
+  public getOffers(): Observable<Offer[]> {
+    return this.http.get<Offer[]>(`${this.host}/offer/find/all`);
+  }
 }
