b5948ba92231af8683e1ca61f2af8f85daf4e4de
shahidfoy <shahidfoy@gmail.com>
2021-10-02 17:55:29 -0600

    implementing basic functions for inqueries component

--- src/app/marketboard/inquiries/inquiries.component.ts
+++ src/app/marketboard/inquiries/inquiries.component.ts
@@ -1,14 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Contract } from 'src/app/model/contract';
+import { User } from 'src/app/model/user';
+import { ContractService } from 'src/app/service/contract.service';
+import { UserService } from 'src/app/service/user.service';
+import { timeFromNow } from 'src/app/shared/shared.utils';
 
 @Component({
   selector: 'app-inquiries',
   templateUrl: './inquiries.component.html',
   styleUrls: ['./inquiries.component.scss'],
 })
-export class InquiriesComponent implements OnInit {
+export class InquiriesComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  contracts: Contract[];
+  usersMap: Map<number, User> = new Map();
+  subscriptions: Subscription[] = [];
 
-  ngOnInit() {}
+  constructor(
+    private contractService: ContractService,
+    private userService: UserService
+  ) { }
 
+  ngOnInit() {
+    this.subscriptions.push(
+      this.contractService.getContracts().subscribe((contracts: Contract[]) => {
+        console.log('contracts', contracts);
+        this.contracts = contracts;
+        this.getContractsUser();
+      })
+     );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
+
+  public getUsername(contracteeId: number): string {
+    return this.usersMap.get(contracteeId) ? this.usersMap.get(contracteeId).username : undefined;
+  }
+
+  public getUserProfileImage(contracteeId: number): string {
+    return this.usersMap.get(contracteeId) ? this.usersMap.get(contracteeId).profileImageUrl : undefined;
+  }
+
+  public timeFromNow(time: Date) {
+    return timeFromNow(time);
+  }
+
+  private getContractsUser() {
+    this.contracts.forEach((contract: Contract) => {
+      this.subscriptions.push(
+        this.userService.findUserById(contract.contracteeId).subscribe((user: User) => {
+          this.usersMap.set(contract.contracteeId, user);
+        }));
+    });
+  }
 }
